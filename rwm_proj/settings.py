923f48286354293e0f38c396b145fac30ab3d198
Varun Bhartia <vbhartia@mba2014.hbs.edu>
2013-06-17 04:53:51 -0400

    updated settings.py for static files

--- rwm_proj/settings.py
+++ rwm_proj/settings.py
@@ -63,8 +63,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-#STATIC_ROOT = ''
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+STATIC_ROOT = ''
+#STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -76,7 +76,8 @@ STATICFILES_DIRS = (
     #'/Users/varunbhartia/development_projects/rwm/rwm_proj/static/rwm_proj',
     
     # For deployment
-    #'static/rwm_proj',
+    #'static/',
+    'static/rwm_proj',
 
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
--- rwm_proj/urls.py
+++ rwm_proj/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+
+# ... the rest of your URLconf goes here ...
+
+urlpatterns += staticfiles_urlpatterns()
 
 urlpatterns = patterns('',
 
